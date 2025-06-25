34546977cb89bd85eb51cc47b310f4cbd54dd1a2
Isaac Corbrey <icorbrey@gmail.com>
2025-06-25 12:37:55 +0000

    postgres: Set up project

--- shell.nix
+++ shell.nix
@@ -2,13 +2,20 @@
   default = pkgs.mkShell {
     buildInputs = with pkgs; [
       docker
+      docker-credential-helpers
       helm
       kind
       kubectl
+      libsecret
+      sqlx-cli
       tilt
     ];
 
     shellHook = ''
+      export DOCKER_CONFIG="$PWD/.docker-config"
+      mkdir -p "$DOCKER_CONFIG"
+      echo '{"auths":{}}' > "$DOCKER_CONFIG/config.json"
+      
       raw_out="$(docker info 2>&1)" || {
         if echo "$raw_out" | grep -qi "permission denied"; then
           echo "🔒 Docker permission denied. Make sure you’re in the 'docker' group and have re-logged in (or run 'newgrp docker')."
